999aba9e0ffa195829d25dff4f7985a9f498080e
Aaryan1311 <ayt131102@gmail.com>
2025-05-07 13:38:21 +0530

    added foreign key constraint in airport table

---
+++ src/migrations/20250507062345-update-city-airport-association.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+      await queryInterface.addConstraint('Airports', {
+      type: 'foreign key',
+      fields: ['cityId'],
+      name: 'custom_fkey_cityId', // Custom name for the constraint
+      references: {
+        table: 'Cities',
+        field: 'id'
+  },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    }
+)},
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Airports', 'custom_fkey_cityId');
+    } 
+};
