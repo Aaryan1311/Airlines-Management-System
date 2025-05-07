31e7619b83e12800e94cfba32cd169b1f62962c1
Aaryan1311 <ayt131102@gmail.com>
2025-05-07 18:51:47 +0530

    added flight model with foreign key constraint

---
+++ src/models/flight.js
@@ -0,0 +1,71 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Flight extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Aeroplane, {
+        foreignKey: 'aeroplaneId',
+        onDelete: 'CASCADE',
+      });
+      this.belongsTo(models.Airport, {
+        foreignKey: 'code',
+        onDelete: 'CASCADE',
+      });
+      this.belongsTo(models.Airport, {
+        foreignKey: 'code',
+        onDelete: 'CASCADE',
+      });
+    }
+  }
+  Flight.init({
+    flightNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    aeroplaneId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    departureAirportId:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    arrivalAirportId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    arrivalTime: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    departureTime: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    boardingGate:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    totalSeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'Flight',
+  });
+  return Flight;
+};
